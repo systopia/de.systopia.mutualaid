5cd79b4201515aeafff1cb2cf03f239d9efa3a8c
B. Endres <endres@systopia.de>
2020-03-23 16:35:28 +0100

    implementing matcher (WIP)

--- CRM/Mutualaid/Matcher.php
+++ CRM/Mutualaid/Matcher.php
@@ -78,18 +78,15 @@ class CRM_Mutualaid_Matcher
             $request_data = [
               'contact_id' => $request->contact_id,
               'location' => [$request->longitude, $request->latitude],
-              'types' => CRM_Utils_Array::explodePadded($request->type_of_help),
-                // TODO: languages
+              'types' => CRM_Utils_Array::explodePadded($request->help_needed),
+              'languages' => CRM_Utils_Array::explodePadded($request->languages),
             ];
 
             // identify potential helpers (apply hard criteria) using SQL query on the helper table
             $potential_helpers = $this->getPotentialHelpers($request_data);
 
             // score potential helpers (apply soft criteria/scoring) and pick best match
-            $helper_data = $this->getBestMatchingHelper(
-              $request_data,
-              $potential_helpers
-            );
+            $helper_data = $this->getBestMatchingHelper($request_data, $potential_helpers);
 
             if ($helper_data) {
                 // there is a helper!
@@ -112,6 +109,73 @@ class CRM_Mutualaid_Matcher
     }
 
     /**
+     * Find a list of potential helpers for the given help request
+     *
+     * @param array $help_request
+     *      help request with fields 'contact_id', 'location', 'types', 'languages'
+     *
+     * @return array
+     *      list of helper structures, containing the fields 'contact_id', 'location', 'types', 'languages'
+     */
+    protected function getPotentialHelpers($help_request)
+    {
+        $potential_helpers = [];
+
+        // generate potential helper query
+        $HELPER_TABLE = $this->getHelperTable();
+        $LONGITUDE    = $help_request['location'][0];
+        $LATITUDE     = $help_request['location'][1];
+
+        // TODO: languages
+
+        // helps matched
+        $HELPS_MATCHED = [];
+        foreach ($help_request['types'] as $help_type_id ) {
+            $help_type_id = (int) $help_type_id;
+            $HELPS_MATCHED[] = "offers_help_{$help_type_id}";
+        }
+        $AT_LEAST_ONE_OFFER_MATCHES = "(" . implode(') OR (', $HELPS_MATCHED). ")";
+
+        // helps offered
+        $help_types = CRM_Mutualaid_Settings::getHelpTypes();
+        $HELP_OFFERED_SELECT_LIST = [];
+        foreach ($help_types as $help_type => $help_name) {
+            $help_type_value = (int) $help_type;
+            $HELP_OFFERED_SELECT_LIST[] = "IF(offers_help_{$help_type_value}, '{$help_type_value},', '')";
+        }
+        $HELP_OFFERED = "CONCAT(". implode(', ', $HELP_OFFERED_SELECT_LIST) . ")";
+
+
+        // build & run query
+        $potential_helper_query = "
+            SELECT
+              contact_id                               AS contact_id,
+              max_distance                             AS max_distance,
+              ((min_longitude +  max_longitude) / 2.0) AS longitude,
+              ((min_latitude + max_latitude) / 2.0)    AS latitude,
+              {$HELP_OFFERED}                          AS offers_help
+            FROM {$HELPER_TABLE}
+            WHERE ({$AT_LEAST_ONE_OFFER_MATCHES})
+              AND min_longitude <= {$LONGITUDE} 
+              AND max_longitude >= {$LONGITUDE} 
+              AND min_latitude  <= {$LATITUDE} 
+              AND max_latitude  >= {$LATITUDE} 
+              AND open_spots > 0
+          ";
+        $potential_helper       = CRM_Core_DAO::executeQuery($potential_helper_query);
+        while ($potential_helper->fetch()) {
+            $potential_helpers[] = [
+                'contact_id'   => $potential_helper->contact_id,
+                'max_distance' => $potential_helper->max_distance,
+                'location'     => [$potential_helper->longitude, $potential_helper->latitude],
+                'offers_help'  => explode(',', trim($this->offers_help, ',)'))
+            ];
+        }
+
+        return $potential_helpers;
+    }
+
+    /**
      * Generate a SQL query to select all open requests,
      *   in order of importance
      *
@@ -120,7 +184,76 @@ class CRM_Mutualaid_Matcher
      */
     protected function getOpenRequestsQuery()
     {
-        // TODO: implement
+        // gather some necessary information
+        $HELP_ASSIGNED_ACTIVE_STATUS_LIST = implode(',', CRM_Mutualaid_Settings::getActiveHelpStatusList());
+        $HELP_TABLE = CRM_Mutualaid_CustomData::getGroupTable('mutualaid_needs_help');
+        $HELP_ASSIGNED_TYPE = CRM_Mutualaid_Settings::getHelpProvidedRelationshipTypeID();
+        $HELP_ASSIGNED_STATUS_FIELD = CRM_Mutualaid_CustomData::getCustomField(
+            'mutualaid',
+            'help_status');
+        $HELP_ASSIGNED_STATUS_COLUMN = $HELP_ASSIGNED_STATUS_FIELD['column_name'];
+        $HELP_ASSIGNED_TABLE = CRM_Mutualaid_CustomData::getGroupTable('mutualaid');
+        $HELP_ASSIGNED_TYPES_FIELD = CRM_Mutualaid_CustomData::getCustomField(
+            'mutualaid',
+            'help_type_provided');
+        $HELP_ASSIGNED_TYPES_COLUMN = $HELP_ASSIGNED_TYPES_FIELD['column_name'];
+
+        $HELP_REQUESTED_FIELD = CRM_Mutualaid_CustomData::getCustomField(
+            'mutualaid_offers_help',
+            'mutualaid_help_offered');
+        $HELP_NEEDED_COLUMN = $HELP_REQUESTED_FIELD['column_name'];
+
+        // TODO: select languages
+        $LANGUAGES_SPOKEN_TABLE = CRM_Mutualaid_CustomData::getGroupTable('mutualaid_language');
+        $LANGUAGES_SPOKEN_FIELD = CRM_Mutualaid_CustomData::getCustomField(
+            'mutualaid_language',
+            'mutualaid_languages');
+        $LANGUAGES_SPOKEN_COLUMN = $LANGUAGES_SPOKEN_FIELD['column_name'];
+
+        // generate filter criteria
+        $help_types = CRM_Mutualaid_Settings::getHelpTypes();
+        $REQUESTS_ALREADY_MATCHED_BY_ASSIGNMENTS = [];
+        foreach ($help_types as $help_type => $help_name) {
+            // each individual clause is true, if $help_type requested AND assigned
+            $help_type_value = (int) $help_type;
+            $token = "CONCAT(0x01, '{$help_type_value}', 0x01)";
+            $REQUESTS_ALREADY_MATCHED_BY_ASSIGNMENTS[] =
+                //     help type not requested       OR            help is assigned and already covers requested type
+                "(LOCATE({$token}, help_needed) = 0) OR ((help_assigned IS NOT NULL) AND (LOCATE({$token}, help_assigned) > 0))";
+        }
+        $ALL_REQUESTS_ALREADY_MATCHED_BY_ASSIGNMENTS = '(' . implode(') AND (', $REQUESTS_ALREADY_MATCHED_BY_ASSIGNMENTS) . ')';
+
+        return "
+          SELECT
+            contact.id                                               AS contact_id,
+            CONCAT(help_assigned_data.{$HELP_ASSIGNED_TYPES_COLUMN}) AS help_assigned,
+            help_requested.{$HELP_NEEDED_COLUMN}                     AS help_needed,
+            languages_spoken.{$LANGUAGES_SPOKEN_COLUMN}              AS languages,
+            address.geo_code_1                                       AS latitude,
+            address.geo_code_2                                       AS longitude
+          FROM civicrm_contact contact
+          LEFT JOIN civicrm_address address                   
+                 ON address.contact_id = contact.id 
+                 AND address.is_primary = 1  
+          LEFT JOIN {$HELP_TABLE}  help_requested              
+                 ON help_requested.entity_id = contact.id
+          LEFT JOIN {$LANGUAGES_SPOKEN_TABLE}  languages_spoken              
+                 ON languages_spoken.entity_id = contact.id
+          LEFT JOIN civicrm_relationship  help_assigned        
+                 ON help_assigned.contact_id_b = contact.id 
+                 AND help_assigned.relationship_type_id = {$HELP_ASSIGNED_TYPE}
+          LEFT JOIN {$HELP_ASSIGNED_TABLE}  help_assigned_data 
+                 ON help_assigned_data.entity_id = help_assigned.id
+                 AND help_assigned_data.{$HELP_ASSIGNED_STATUS_COLUMN} IN ({$HELP_ASSIGNED_ACTIVE_STATUS_LIST})
+                                   
+          WHERE (contact.is_deleted IS NULL OR contact.is_deleted = 0)
+            AND address.geo_code_1 IS NOT NULL
+            AND address.geo_code_2 IS NOT NULL
+            AND help_requested.{$HELP_NEEDED_COLUMN} IS NOT NULL 
+            AND help_requested.{$HELP_NEEDED_COLUMN} <> ''
+          GROUP BY contact.id
+          HAVING NOT ({$ALL_REQUESTS_ALREADY_MATCHED_BY_ASSIGNMENTS})
+        ";
     }
 
     /**
@@ -137,15 +270,16 @@ class CRM_Mutualaid_Matcher
             //  contact_id, latitude, longitude, open spots, help_offered (true/false columns), languages (true/false columns) late
 
             // gather some necessary information
-            $HELP_PROVIDED_ACTIVE_STATUS_LIST = implode(
-              ',',
-              CRM_Mutualaid_Settings::getActiveHelpStatusList()
-            );
+            $HELP_PROVIDED_ACTIVE_STATUS_LIST = implode(',', CRM_Mutualaid_Settings::getActiveHelpStatusList());
             $MAX_JOBS_FIELD = CRM_Mutualaid_CustomData::getCustomField(
               'mutualaid_offers_help',
-              'mutualaid_max_persons'
-            );
+              'mutualaid_max_persons');
             $MAX_JOBS_COLUMN = $MAX_JOBS_FIELD['column_name'];
+            $MAX_DISTANCE_FIELD = CRM_Mutualaid_CustomData::getCustomField(
+                'mutualaid_offers_help',
+                'mutualaid_max_distance');
+            $MAX_DISTANCE_COLUMN = $MAX_DISTANCE_FIELD['column_name'];
+
             $HELP_TABLE = CRM_Mutualaid_CustomData::getGroupTable(
               'mutualaid_offers_help'
             );
@@ -173,6 +307,10 @@ class CRM_Mutualaid_Matcher
               $HELP_OFFERED_SELECTS .= ',';
             }
 
+            // TODO: calculate proper min/max factors (meters per degree)
+            $LATITUDE_FACTOR  = 111000.1;
+            $LONGITUDE_FACTOR = 73000.1;
+
             // build languages spoken
             $HELP_LANGUAGES_SPOKEN = ''; // TODO
 
@@ -182,10 +320,18 @@ class CRM_Mutualaid_Matcher
                 contact.id                   AS contact_id,
                 (help_offered.{$MAX_JOBS_COLUMN} - COUNT(help_provided_data.id))      
                                              AS open_spots,
+                help_offered.{$MAX_DISTANCE_COLUMN}      
+                                             AS max_distance,
                 {$HELP_OFFERED_SELECTS}
                 {$HELP_LANGUAGES_SPOKEN}
-                address.geo_code_1           AS latitude,
-                address.geo_code_2           AS longitude
+                (address.geo_code_1 - (help_offered.{$MAX_DISTANCE_COLUMN} / {$LATITUDE_FACTOR}))            
+                                             AS min_latitude,
+                (address.geo_code_1 + (help_offered.{$MAX_DISTANCE_COLUMN} / {$LATITUDE_FACTOR}))
+                                             AS max_latitude,
+                (address.geo_code_2 - (help_offered.{$MAX_DISTANCE_COLUMN} / {$LONGITUDE_FACTOR}))           
+                                             AS min_longitude,
+                (address.geo_code_2 + (help_offered.{$MAX_DISTANCE_COLUMN} / {$LONGITUDE_FACTOR}))           
+                                             AS max_longitude
               FROM civicrm_contact contact
               LEFT JOIN civicrm_address address                   
                      ON address.contact_id = contact.id 
@@ -197,11 +343,10 @@ class CRM_Mutualaid_Matcher
                      AND help_provided.relationship_type_id = {$HELP_PROVIDED_TYPE}
               LEFT JOIN {$HELP_PROVIDED_TABLE}  help_provided_data 
                      ON help_provided_data.entity_id = help_provided.id
-                     AND help_provided_data.{$HELP_PROVIDED_STATUS_COLUMN} IN ({$HELP_PROVIDED_ACTIVE_STATUS_LIST}) 
+                     AND help_provided_data.{$HELP_PROVIDED_STATUS_COLUMN} IN ({$HELP_PROVIDED_ACTIVE_STATUS_LIST})
               WHERE (contact.is_deleted IS NULL OR contact.is_deleted = 0)
                 AND address.geo_code_1 IS NOT NULL
                 AND address.geo_code_2 IS NOT NULL
-                AND help_provided_data.id IS NOT NULL  -- only count active help_provided relationships
                 AND help_offered.{$HELP_OFFERED_TYPE} IS NOT NULL 
                 AND help_offered.{$HELP_OFFERED_TYPE} <> ''
               GROUP BY contact.id
@@ -215,8 +360,10 @@ class CRM_Mutualaid_Matcher
             // add indexes
             $helper_table_name = $this->helper_table->getName();
             CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX contact_id(contact_id)");
-            CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX latitude(latitude)");
-            CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX longitude(longitude)");
+            CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX max_latitude(max_latitude)");
+            CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX min_latitude(min_latitude)");
+            CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX max_longitude(max_longitude)");
+            CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX min_longitude(min_longitude)");
             foreach ($help_types as $help_type => $help_name) {
                 $help_type_value = (int) $help_type;
                 CRM_Core_DAO::executeQuery("ALTER TABLE `{$helper_table_name}` ADD INDEX offers_help_{$help_type_value}(offers_help_{$help_type_value})");
