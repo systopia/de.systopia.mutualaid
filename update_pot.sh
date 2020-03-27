ebfbf1596c472d65abe5c50488837675eded4b93
B. Endres <endres@systopia.de>
2020-03-27 10:32:24 +0100

    fixed l10n

--- update_pot.sh
+++ update_pot.sh
@@ -2,4 +2,6 @@
 
 l10n_tools="../civi_l10n_tools"
 
-${l10n_tools}/bin/create-pot-files-extensions.sh de.systopia.mutualaid ./ l10n
+${l10n_tools}/bin/create-pot-files-extensions.sh mutualaid ./ l10n
+
+cat l10n/mutualaid_custom_data.pot >> l10n/mutualaid.pot
